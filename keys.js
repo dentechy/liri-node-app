3ef2970beda3c752199815efc65f6b004d542982
Dennis Feliciano <dennis.feliciano5@gmail.com>
2017-06-17 19:03:22 -0400

    continues progress

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'g2qZCE1sso2lahPCxfTVvafES',
+  consumer_secret: 'DR5aqKhVSueVvvGnlXe2Qwl4MGH7XF4nYtGTjadHHZVqFqLWXG',
+  access_token_key: '876195184249368577-n3g4st0e1CnD7eKZRm4OteE6sprcm5V', 
+  access_token_secret: 'whtYcU5OUxeu2N3DN3vUKGFJZG2U3yf2EYyzqU2seOr94',
+}
---
+++ liri.js
@@ -0,0 +1,6 @@
+//Needs to read the keys from the keys.js file
+//functions required: my-tweets, spotify-this-song, movie-this, do-what-it-says
+//spotify won't work due to a change in their api
+
+const keys = require('./keys.js');
+
