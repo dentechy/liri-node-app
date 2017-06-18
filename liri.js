78f00f9c3dc4d427a99f589fee410cca4d8c60d2
Dennis Feliciano <dennis.feliciano5@gmail.com>
2017-06-18 19:25:57 -0400

    adds functional twitter cli application, grabs tweets + time

--- liri.js
+++ liri.js
@@ -2,5 +2,31 @@
 //functions required: my-tweets, spotify-this-song, movie-this, do-what-it-says
 //spotify won't work due to a change in their api
 
+const fs = require('fs');
 const keys = require('./keys.js');
+const twitter = require('twitter');
 
+var userInput = process.argv[2];
+var movieInput = process.argv[3]
+
+var client = new twitter({
+  consumer_key: keys.twitterKeys.consumer_key,
+  consumer_secret: keys.twitterKeys.consumer_secret,
+  access_token_key: keys.twitterKeys.access_token_key,
+  access_token_secret: keys.twitterKeys.access_token_secret
+});
+
+if (userInput === 'my-tweets') {
+var params = {screen_name: 'nodetweety'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    for (i = 0; i < tweets.length; i++) {
+      console.log(tweets[i].created_at + " " + tweets[i].text);
+    }
+  } 
+});
+}
+
+else if (userInput === 'movie-this') {
+
+}
