50fe9436d8493828ae164dd5de0100dc7a243564
Dennis Feliciano <dennis.feliciano5@gmail.com>
2017-06-19 01:35:41 -0400

    minor fix in presentation of tweets

--- liri.js
+++ liri.js
@@ -21,7 +21,7 @@ var params = {screen_name: 'nodetweety'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
     for (i = 0; i < tweets.length; i++) {
-      console.log(tweets[i].created_at + " " + tweets[i].text);
+      console.log(tweets[i].created_at + ": " + tweets[i].text);
     }
   } 
 });
